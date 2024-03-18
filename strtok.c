9727f02e853dc1a1831b98b939f868950f262266
ronco78 <157357735+ronco78@users.noreply.github.com>
2024-03-18 13:50:33 +0200

    Add files via upload

---
+++ strtok.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+static char *olds;
+
+size_t strlen( const char *str )
+{
+	size_t len;
+
+	while(str[len])
+		len++;
+
+	return len;
+}
+
+char *strpbrk(const char *s, const char *accept)
+{
+	while (*s != '\0') {
+		const char *a = accept;
+		while (*a != '\0')
+			if (*++a == *s)
+				return (char *) s;
+		++s;
+	}
+
+	return NULL;
+}
+
+char *strtok(char *s, const char *delim)
+{
+	char *token;
+
+	if (s == NULL)
+		s = olds;
+
+	token = s;
+	s = strpbrk(token, delim);
+	if (s == NULL)
+		olds = token + strlen(token);
+	else {
+		*s = '\0';
+		olds = s + 1;
+	}
+
+	return token;
+}
+
+int main(int argc, char **argv)
+{
+	char *token;
+
+	token = strtok(argv[0], argv[1]);
+	while (token) {
+		printf("%s\n", token);
+		token = strtok(NULL, argv[1]);
+	}
+}
+
